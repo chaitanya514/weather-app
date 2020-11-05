8d900457725b9620981bf7ef9442ba51b0f43cfc
Chaitanya Patel <patelchaitanya514@gmail.com>
2020-11-05 07:39:29 -0500

    api fetch works

--- src/App.js
+++ src/App.js
@@ -5,21 +5,65 @@ class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      status : 'init',
-      isLoaded:false,
-      weatherData:null
+      status: 'init',
+      isLoaded: false,
+      weatherData: null
     }
+
   }
+
+  abortController = new AbortController();
+  controllerSignal = this.abortController.signal;
 
   getWeatherData = () => {
-      const weatherApi  = `http://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=${process.env.REACT_APP_WEATHER_KEY}`;
+    const weatherApi = 'http://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=f7e7395da34f297d325355616a297568';
 
-      fetch(weatherApi,{signal:this.controllerSignal})
-      .then(response=>response.json())
-      .then((result)=>{
-        console.log("weather api result:",result)
-      })
+    fetch(weatherApi, { signal: this.controllerSignal })
+      .then(response => response.json())
+      .then((result) => {
+        console.log("weather api result:", result);
+
+        const { name } = result;
+        const { country } = result.sys;
+        const { temp, temp_min, temp_max, feels_like, humidity } = result.main;
+        const { description, icon } = result.weather[0];
+        const { speed, deg } = result.wind;
+
+        this.setState = ({
+          isLoaded: true,
+          weatherData: {
+            name,
+            country,
+            description,
+            icon,
+            temp: temp.toFixed(1),
+            feels_like: feels_like.toFixed(1),
+            temp_min: temp_min.toFixed(1),
+            temp_max: temp_max.toFixed(1),
+            speed,
+            deg,
+            humidity
+          }
+        })
+
+      },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          })
+        }
+      );
   }
+
+
+  componentDidMount() {
+    this.getWeatherData();
+  }
+  componentWillUnmount(){
+    this.abortController.abort();
+  }
+
 
   render() {
     return (
